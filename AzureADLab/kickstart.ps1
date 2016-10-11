949daef9fba35567ab93d471f0a4ba8b119a4624
Jared Haight <jared@photosandtext.com>
2016-10-10 23:27:22 -0400

    asdf

--- AzureADLab/kickstart.ps1
+++ AzureADLab/kickstart.ps1
@@ -12,39 +12,34 @@ function Get-RandomString ($length) {
 }
 
 # DC Variables
-$URI  = 'https://raw.githubusercontent.com/jaredhaight/AzureADLab/master/AzureADLab/azuredeploy.json'
-$_artifactsLocation = "https://raw.githubusercontent.com/jaredhaight/AzureADLab/master/AzureADLab/"
-$location  = 'eastus2'
-$locationName = "East US"
-$resourceGroupName    = 'evil.training'
-$studentCode = "a" + (Get-RandomString 6)
-$adminUserName = "EvilAdmin"
-$adminPasswordPlainText = "Beak-Today#Favor#Manufacture&0s"
-$adminPassword = ConvertTo-SecureString $adminPasswordPlainText -AsPlainText -Force
-$domainName = "ad.evil.training"
-$dnsPrefix = $studentCode
-$storageAccountName = $studentCode+"storage"    # Lowercase required
-$adVMName = $studentCode+"-dc01"
-$adAvailabilitySetName = $studentCode+"AvailSet"
-$adNicName = $studentCode + "-dc01nic"
-$adNicIPAddress = "10.0.0.4"
-$adSubnetName = $studentCode+"subnet"
-$adSubnetAddressPrefix = "10.0.0.0/24"
-$virtualNetworkName = $studentCode+"vnet"
+$URI                        = 'https://raw.githubusercontent.com/jaredhaight/AzureADLab/master/AzureADLab/azuredeploy.json'
+$_artifactsLocation         = "https://raw.githubusercontent.com/jaredhaight/AzureADLab/master/AzureADLab/"
+$location                   = 'eastus2'
+$locationName               = "East US"
+$resourceGroupName          = 'evil.training'
+$studentCode                = "a" + (Get-RandomString 6)
+$adminUserName              = "EvilAdmin"
+$adminPassword              = "Beak-Today#Favor#Manufacture&0s"
+$domainName                 = "ad.evil.training"
+$dnsPrefix                  = $studentCode
+$storageAccountName         = $studentCode+"storage"    # Lowercase required
+$adVMName                   = $studentCode+"-dc01"
+$adAvailabilitySetName      = $studentCode+"AvailSet"
+$adNicName                  = $studentCode + "-dc01nic"
+$adNicIPAddress             = "10.0.0.4"
+$adSubnetName               = $studentCode+"subnet"
+$adSubnetAddressPrefix      = "10.0.0.0/24"
+$virtualNetworkName         = $studentCode+"vnet"
 $virtualNetworkAddressRange = "10.0.0.0/16"
-$publicIPAddressName = $studentCode+"-dc01pip"
+$publicIPAddressName        = $studentCode+"-dc01pip"
+$adVmSize                   = "Basic_A1"
 
 # Client Vars
-$clientURI = "https://raw.githubusercontent.com/jaredhaight/AzureADLab/master/AzureVMDomainJoin/azuredeploy.json"
-$existingVNETName = $virtualNetworkName
-$clientDnsPrefix = $studentCode+"-home"
-$existingSubnetName = $adSubnetName
-$domainToJoin = $domainName
-$domainUsername = $adminUserName
-$domainPassword = $adminPasswordPlainText
-$vmAdminUsername = "homeAdmin"
-$vmAdminPassword = "Password123"
-$clientNicIpAddress = "10.0.0.10"
+$clientDnsPrefix            = $studentCode+"-home"
+$vmAdminUsername            = "homeAdmin"
+$vmAdminPassword            = "Password123"
+$clientNicIpAddress         = "10.0.0.10"
+$clientVMSize               = "Basic_A2"
 
 # Check that the public dns $addnsName is available
 try {
@@ -90,6 +85,7 @@ $MyParams = @{
   adNicName                   = $adNicName
   adNicIpAddress              = $adNicIPAddress
   adVMName                    = $adVMName
+  adVMSize                    = $adVMSize
   adSubnetName                = $adSubnetName
   publicIPAddressName         = $publicIPAddressName
   adAvailabilitySetname       = $adAvailabilitySetName
@@ -98,21 +94,22 @@ $MyParams = @{
   _artifactsLocation          = $_artifactsLocation
   existingVNETName            = $existingVNETName
   existingSubnetName          = $existingSubnetName
-  clientDnsPrefix              = $clientDnsPrefix
+  clientDnsPrefix             = $clientDnsPrefix
   domainToJoin                = $domainToJoin
   domainUserName              = $domainUsername
   domainPassword              = $domainPassword
   vmAdminUsername             = $vmAdminUsername
   vmAdminPassword             = $vmAdminPassword
   clientNicIpAddress          = $clientNicIpAddress
+  clientVmSize                = $clientVMSize
 }
 
 # Splat the parameters on New-AzureRmResourceGroupDeployment  
 $SplatParams = @{
-  TemplateUri             = $URI 
-  ResourceGroupName       = $resourceGroupName 
-  TemplateParameterObject = $MyParams
-  Name                    = 'EVILTraining'
+  TemplateUri                 = $URI 
+  ResourceGroupName           = $resourceGroupName 
+  TemplateParameterObject     = $MyParams
+  Name                        = 'EVILTraining'
 }
 
 # This takes ~30 minutes
