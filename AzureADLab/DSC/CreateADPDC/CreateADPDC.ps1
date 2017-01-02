5f2ae93140f9ff5449cb5cfc369289d0033e8dbc
Jared Haight <jared@photosandtext.com>
2017-01-01 22:17:41 -0500

    updating dsc

--- AzureADLab/DSC/CreateADPDC/CreateADPDC.ps1
+++ AzureADLab/DSC/CreateADPDC/CreateADPDC.ps1
@@ -1,106 +1,106 @@
 ﻿configuration CreateADPDC 
 { 
-   param 
-   ( 
-        [Parameter(Mandatory)]
-        [String]$DomainName,
+  param 
+  ( 
+  [Parameter(Mandatory)]
+  [String]$DomainName,
 
-        [Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$Admincreds,
+  [Parameter(Mandatory)]
+  [System.Management.Automation.PSCredential]$Admincreds,
 
-        [Int]$RetryCount=20,
-        [Int]$RetryIntervalSec=30
-    ) 
-    
-    Import-DscResource -ModuleName xActiveDirectory,xDisk, xNetworking, cDisk, PSDesiredStateConfiguration
-    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
-    $InterfaceAlias=$($Interface.Name)
+  [Int]$RetryCount=20,
+  [Int]$RetryIntervalSec=30
+  ) 
 
-    Node localhost
+  Import-DscResource -ModuleName xActiveDirectory, xDisk, xNetworking, cDisk, PSDesiredStateConfiguration
+  [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+  $Interface=Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+  $InterfaceAlias=$($Interface.Name)
+
+  Node localhost
+  {
+    Script AddADDSFeature {
+      SetScript = {
+        Add-WindowsFeature "AD-Domain-Services" -ErrorAction SilentlyContinue   
+      }
+      GetScript =  { @{} }
+      TestScript = { $false }
+    }
+
+    WindowsFeature DNS 
+    { 
+      Ensure = "Present" 
+      Name = "DNS"		
+    }
+
+    Script script1
     {
-        Script AddADDSFeature {
-            SetScript = {
-                Add-WindowsFeature "AD-Domain-Services" -ErrorAction SilentlyContinue   
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-        }
-	
-	    WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"		
-        }
+      SetScript =  { 
+        Set-DnsServerDiagnostics -All $true
+        Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+      }
+      GetScript =  { @{} }
+      TestScript = { $false }
+      DependsOn = "[WindowsFeature]DNS"
+    }
 
-        Script script1
-	    {
-      	    SetScript =  { 
-		        Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-	        DependsOn = "[WindowsFeature]DNS"
-        }
+    WindowsFeature DnsTools
+    {
+      Ensure = "Present"
+      Name = "RSAT-DNS-Server"
+    }
 
-	    WindowsFeature DnsTools
-	    {
-	        Ensure = "Present"
-            Name = "RSAT-DNS-Server"
-	    }
+    xDnsServerAddress DnsServerAddress 
+    { 
+      Address        = '127.0.0.1' 
+      InterfaceAlias = $InterfaceAlias
+      AddressFamily  = 'IPv4'
+      DependsOn = "[WindowsFeature]DNS"
+    }
 
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = $InterfaceAlias
-            AddressFamily  = 'IPv4'
-	        DependsOn = "[WindowsFeature]DNS"
-        }
+    xWaitforDisk Disk2
+    {
+      DiskNumber = 2
+      RetryIntervalSec =$RetryIntervalSec
+      RetryCount = $RetryCount
+    }
 
-        xWaitforDisk Disk2
-        {
-             DiskNumber = 2
-             RetryIntervalSec =$RetryIntervalSec
-             RetryCount = $RetryCount
-        }
+    cDiskNoRestart ADDataDisk
+    {
+      DiskNumber = 2
+      DriveLetter = "F"
+    }
 
-        cDiskNoRestart ADDataDisk
-        {
-            DiskNumber = 2
-            DriveLetter = "F"
-        }
+    WindowsFeature ADDSInstall 
+    { 
+      Ensure = "Present" 
+      Name = "AD-Domain-Services"
+      DependsOn="[cDiskNoRestart]ADDataDisk", "[Script]AddADDSFeature"
+    } 
 
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
-            Name = "AD-Domain-Services"
-	        DependsOn="[cDiskNoRestart]ADDataDisk", "[Script]AddADDSFeature"
-        } 
-         
-        xADDomain FirstDS 
-        {
-            DomainName = $DomainName
-            DomainAdministratorCredential = $DomainCreds
-            SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "F:\NTDS"
-            LogPath = "F:\NTDS"
-            SysvolPath = "F:\SYSVOL"
-	        DependsOn = "[WindowsFeature]ADDSInstall"
-        } 
-        xADGroup LocalAdmins
-        {
-           GroupName = "LocalAdmins"
-           GroupScope = "Global"
-           Category = "Security"
-           Description = "Group for Local Admins"
-           Ensure = 'Present'
-        }
+    xADDomain FirstDS 
+    {
+      DomainName = $DomainName
+      DomainAdministratorCredential = $DomainCreds
+      SafemodeAdministratorPassword = $DomainCreds
+      DatabasePath = "F:\NTDS"
+      LogPath = "F:\NTDS"
+      SysvolPath = "F:\SYSVOL"
+      DependsOn = "[WindowsFeature]ADDSInstall"
+    } 
+    xADGroup LocalAdmins
+    {
+      GroupName = "LocalAdmins"
+      GroupScope = "Global"
+      Category = "Security"
+      Description = "Group for Local Admins"
+      Ensure = 'Present'
+    }
 
-        LocalConfigurationManager 
-        {
-            ConfigurationMode = 'ApplyOnly'
-            RebootNodeIfNeeded = $true
-        }
-   }
+    LocalConfigurationManager 
+    {
+      ConfigurationMode = 'ApplyOnly'
+      RebootNodeIfNeeded = $true
+    }
+  }
 } 
