63a985dee1baafcadcca38174f90bb206fbe8a03
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2018-02-27 19:27:10 -0800

    adding files url to server dsc

--- PowerShellClassLab/DSC/ServerConfig/ServerConfig.ps1
+++ PowerShellClassLab/DSC/ServerConfig/ServerConfig.ps1
@@ -1,84 +1,79 @@
 configuration ServerConfig 
 {
-   param 
-   ( 
-        [Parameter(Mandatory)]
-        [String]$DomainName,
-        [Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$Admincreds
-    )
+  param 
+  ( 
+    [Parameter(Mandatory)]
+    [String]$DomainName,
+    [Parameter(Mandatory)]
+    [System.Management.Automation.PSCredential]$Admincreds,
+    [Parameter(Mandatory)]
+    [String]$filesUrl
+  )
   
   Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[Start] Got FileURL: $filesUrl"
-  Import-DscResource -ModuleName xSmbShare,PSDesiredStateConfiguration,xWindowsUpdate,cChoco
+  Import-DscResource -ModuleName xSmbShare, PSDesiredStateConfiguration, xWindowsUpdate, cChoco
   Import-DscResource -ModuleName xComputerManagement -Name xScheduledTask
 
   [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
   
   Node localhost 
   {
-    WindowsFeature FileServer
-    {
-        Ensure = "Present" 
-        Name = "FS-FileServer"
+    WindowsFeature FileServer {
+      Ensure = "Present" 
+      Name   = "FS-FileServer"
     }
-    WindowsFeature WebServer
-    {
-        Ensure = "Present" 
-        Name = "Web-Server"
+    WindowsFeature WebServer {
+      Ensure = "Present" 
+      Name   = "Web-Server"
     }
-    Script DisableFirewall
-    {
-        SetScript =  { 
-            Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[DisableFirewall] Running.."
-            Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-        }
-        GetScript =  { @{} }
-        TestScript = { $false }
+    Script DisableFirewall {
+      SetScript  = { 
+        Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[DisableFirewall] Running.."
+        Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+      }
+      GetScript  = { @{} }
+      TestScript = { $false }
     }
 
-    Group AddLocalAdminsGroup
-    {
-        GroupName='Administrators'   
-        Ensure= 'Present'             
-        MembersToInclude= "$DomainName\LocalAdmins"
-        Credential = $DomainCreds    
-        PsDscRunAsCredential = $DomainCreds
+    Group AddLocalAdminsGroup {
+      GroupName            = 'Administrators'   
+      Ensure               = 'Present'             
+      MembersToInclude     = "$DomainName\LocalAdmins"
+      Credential           = $DomainCreds    
+      PsDscRunAsCredential = $DomainCreds
     }
-    Group AddRDPAccessGroup
-    {
-        GroupName='Remote Desktop Users'   
-        Ensure= 'Present'             
-        MembersToInclude= "$DomainName\RDP Access"
-        Credential = $DomainCreds    
-        PsDscRunAsCredential = $DomainCreds
+    Group AddRDPAccessGroup {
+      GroupName            = 'Remote Desktop Users'   
+      Ensure               = 'Present'             
+      MembersToInclude     = "$DomainName\RDP Access"
+      Credential           = $DomainCreds    
+      PsDscRunAsCredential = $DomainCreds
     }
-    File DataFolder
-    {
-        Ensure = "Present"
-        DestinationPath = "C:\Data"
-        Type = "Directory"
-        Force = $true
+    File DataFolder {
+      Ensure          = "Present"
+      DestinationPath = "C:\Data"
+      Type            = "Directory"
+      Force           = $true
     }
     xSmbShare DataShare
     {
-        Ensure = "Present" 
-        Name   = "Data"
-        Path = "C:\Data"  
-        FullAccess = "Everyone"
-        DependsOn = "[File]DataFolder"
+      Ensure     = "Present" 
+      Name       = "Data"
+      Path       = "C:\Data"  
+      FullAccess = "Everyone"
+      DependsOn  = "[File]DataFolder"
     }    
-    Script UpdateHelp
-    {
-        SetScript =  { 
-            Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[UpdateHelp] Running.."
-            Update-Help -Force -ErrorAction SilentlyContinue
-        }
-        GetScript =  { @{} }
-        TestScript = { $false }
+    Script UpdateHelp {
+      SetScript  = { 
+        Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[UpdateHelp] Running.."
+        Update-Help -Force -ErrorAction SilentlyContinue
+      }
+      GetScript  = { @{} }
+      TestScript = { $false }
     }   
     cChocoInstaller installChoco
     {
-        InstallDir = "c:\choco"
+      InstallDir = "c:\choco"
     }
     cChocoPackageInstaller installBGinfo
     {
@@ -86,29 +81,27 @@ configuration ServerConfig
       DependsOn   = "[cChocoInstaller]installChoco"
       AutoUpgrade = $True
     }
-    Script DownloadBGIFile
-    {
-        SetScript =  { 
-            $file = $using:filesUrl + 'LAB.bgi'
-            Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[DownloadBGIFile] Downloading $file"
-            Invoke-WebRequest -Uri $file -OutFile C:\LAB.bgi
-        }
-        GetScript =  { @{} }
-        TestScript = { 
-            Test-Path C:\LAB.bgi
-         }
+    Script DownloadBGIFile {
+      SetScript  = { 
+        $file = $using:filesUrl + 'LAB.bgi'
+        Add-Content -Path "C:\Windows\Temp\jah-dsc-log.txt" -Value "[DownloadBGIFile] Downloading $file"
+        Invoke-WebRequest -Uri $file -OutFile C:\LAB.bgi
+      }
+      GetScript  = { @{} }
+      TestScript = { 
+        Test-Path C:\LAB.bgi
+      }
     }
     xScheduledTask xScheduledTaskLogonAdd
     {
-        TaskName           = 'BGinfo'
-        ActionExecutable   = 'C:\ProgramData\chocolatey\bin\Bginfo.exe'
-        ActionArguments    = 'C:\LAB.bgi'
-        ScheduleType       = 'AtLogOn'
+      TaskName         = 'BGinfo'
+      ActionExecutable = 'C:\ProgramData\chocolatey\bin\Bginfo.exe'
+      ActionArguments  = 'C:\LAB.bgi'
+      ScheduleType     = 'AtLogOn'
     }
-    LocalConfigurationManager 
-    {
-        ConfigurationMode = 'ApplyOnly'
-        RebootNodeIfNeeded = $true
+    LocalConfigurationManager {
+      ConfigurationMode  = 'ApplyOnly'
+      RebootNodeIfNeeded = $true
     }
   }
 }
